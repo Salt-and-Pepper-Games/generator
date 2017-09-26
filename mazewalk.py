d4be440f8f2e2b530d6de868f15812c1f0d1e050
Peter Stefek <peterlikesemail@gmail.com>
2017-09-25 21:54:21 -0400

    added most of the drill logic

--- mazewalk.py
+++ mazewalk.py
@@ -49,7 +49,7 @@ def build_grid(width, height):
 					grid[x][y][color].planar_neighbors.append(grid[neighbor[0]][neighbor[1]][color])
 	return grid
 
-# returns the true neighbors (accounting for switches) of a node
+# returns a list of the true neighbors (accounting for switches) of a node
 def get_neighbors(node):
 	neighbors = []
 	for neighbor in node.planar_neighbors:
@@ -59,8 +59,32 @@ def get_neighbors(node):
 			neighbors.append(neighbor)
 	return neighbors
 
-def count_visited(neighbors):
-	return reduce(lambda total, curr: total + int(curr.visited), 0)
+def can_reach_me(node):
+	return node.planar_neighbors
+
+def i_can_reach(node):
+	return get_neighbors(node)
+
+# if we drill out this node will there be a loop
+def check_drill_creates_loop(node):
+	visited_in_nodes = set(filter(lambda x:x.visited, can_reach_me(node)))
+	visited_out_nodes = set(filter(lambda x:x.visited, i_can_reach(node)))
+	return len(in_nodes) > 0 and len(out_nodes) > 0 and len(in_nodes.difference(out_nodes)) > 1
+
+def check_drill_creates_unpassables(node):
+	# only check planar neighbors because switch neighbors already have column taken and cannot become un-passable hence no need to resolve them
+	filled_planar_neighbors = filter(lambda x:not x.column_taken, node.planar_neighbors)
+	for neighbor in filled_planar_neighbors:
+		is_black_block = True
+		for node_in_column in neighbor.entire_column:
+			empty_in_nodes = set(filter(lambda x:x.empty, can_reach_me(neighbors)))
+			empty_out_nodes = set(filter(lambda x:x.empty, i_can_reach(neighbors)))
+			if not (len(in_nodes) > 0 and len(out_nodes) > 0 and len(in_nodes.difference(out_nodes)) > 1):
+				is_black_block = False
+				break
+		if is_black_block == True:
+			return True
+	return False
 
 def generate(width, height, x_start, y_start, empty_prob=.5, switch_prob=.2, drill_prob=.3):
 	grid = build_grid(width, height)
@@ -78,9 +102,35 @@ def generate(width, height, x_start, y_start, empty_prob=.5, switch_prob=.2, dri
 		
 
 def is_drill_valid(start, finish):
+	# we only drill on a filled column which is not taken
 	if start.visited and not finish.column_taken and not finish.visited:
 		# check that it won't create a loop
-
+		return not (check_drill_creates_loop(finish) or check_drill_creates_unpassables())
 	else:
 		return False;
 
+def mark_drill_visited_and_get_walls(node):
+	new_walls = []
+	visited_nodes = []
+
+	mark_drill_visited(node)
+	visited_nodes.append(node)
+
+	visited_nodes += visit_empty_neighbors(node)
+
+	for new_node in visited_nodes:
+		new_walls += get_new_walls(new_node)
+
+	return new_walls
+
+
+def visit_empty_neighbors(node):
+	# TODO flood fill empty neighbors and mark visited, return list of all nodes marked visited
+
+def mark_drill_visited(node):
+	node.visited = True
+	node.empty = True
+	for col_neighbor in node.entire_column:
+		col_neighbor.column_taken = True
+
+def get_new_walls(node):
