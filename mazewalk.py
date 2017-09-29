1772364f62d4e0f53b64ad081ca42b6d8fb2201e
Simon Ever-Hale <simoneverhale@gmail.com>
2017-09-29 16:29:51 -0400

    first try at switches

--- mazewalk.py
+++ mazewalk.py
@@ -72,7 +72,8 @@ def i_can_reach(node):
 
 def generate(width, height, x_start, y_start, empty_prob=.5, switch_prob=.2, drill_prob=.3):
 	grid = build_grid(width, height)
-	walls = mark_drill_visited_and_get_walls(grid[x_start][y_start][0])
+	mark_drill_visited(grid[x_start][y_start][0])
+	walls = mark_neighbors_visited_and_get_walls(grid[x_start][y_start][0])
 	while len(walls) > 0:
 		#O(1) randomization scheme
 		swap_indx = random.randint(0, len(walls) - 1)
@@ -87,7 +88,13 @@ def generate(width, height, x_start, y_start, empty_prob=.5, switch_prob=.2, dri
 		#TODO do the rest
 		if wall_type == DRILL:
 			if is_drill_valid(start, finish):
-				walls += mark_drill_visited_and_get_walls(finish)
+				mark_drill_visited(finish)
+				walls += mark_neighbors_visited_and_get_walls(finish)
+		elif wall_type == SWITCH:
+			switch_color = start.color ^ finish.color
+			if is_switch_valid(start, finish, switch_color):
+				mark_switch_visited(finish, switch_color)
+				walls += mark_neighbors_visited_and_get_walls(finish)
 	return grid
 
 """
@@ -112,6 +119,47 @@ def visit_empty_neighbors(node):
 			visited_nodes.append(neighbor)
 			visited_nodes += visit_empty_neighbors(neighbor)
 	return visited_nodes
+
+def mark_neighbors_visited_and_get_walls(node):
+	new_walls = []
+	visited_nodes = []
+
+	visited_nodes.append(node)
+
+	visited_nodes += visit_empty_neighbors(node)
+
+	for new_node in visited_nodes:
+		new_walls += get_new_walls(new_node)
+
+	return new_walls
+
+
+# if we drill out this node will there be a loop
+def check_creates_loop(node, switched_node=None):
+	if switched_node == None:
+		switched_node = node
+	visited_in_nodes = set(filter(lambda x:x.visited, can_reach_me(switched_node)))
+	visited_out_nodes = set(filter(lambda x:x.visited, i_can_reach(node)))
+	return len(visited_in_nodes) > 0 and len(visited_out_nodes) > 0 and len(visited_in_nodes.union(visited_out_nodes)) > 1
+
+def check_creates_unpassables(node, switched_node=None):
+	if switched_node == None:
+		switched_node = node
+	# only check planar neighbors because switch neighbors already have column taken and cannot become un-passable hence no need to resolve them
+	filled_planar_neighbors = filter(lambda x:not x.column_taken, node.planar_neighbors)
+	for neighbor in filled_planar_neighbors:
+		is_black_block = True
+		for node_in_column in neighbor.entire_column:
+			empty_in_nodes = set(filter(lambda x:x.empty, can_reach_me(node_in_column)))
+			empty_in_nodes.add(node)
+			empty_out_nodes = set(filter(lambda x:x.empty, i_can_reach(node_in_column)))
+			empty_out_nodes.add(switched_node)
+			if not (len(empty_in_nodes) > 0 and len(empty_out_nodes) > 0 and len(empty_in_nodes.union(empty_out_nodes)) > 1):
+				is_black_block = False
+				break
+		if is_black_block == True:
+			return True
+	return False
 	
 """
 --------------\
@@ -122,23 +170,9 @@ def is_drill_valid(start, finish):
 	# we only drill on a filled column which is not taken
 	if start.visited and not finish.column_taken and not finish.visited:
 		# check that it won't create a loop
-		return not (check_drill_creates_loop(finish) or check_drill_creates_unpassables(finish))
+		return not (check_creates_loop(finish) or check_creates_unpassables(finish))
 	else:
 		return False;
-
-def mark_drill_visited_and_get_walls(node):
-	new_walls = []
-	visited_nodes = []
-
-	mark_drill_visited(node)
-	visited_nodes.append(node)
-
-	visited_nodes += visit_empty_neighbors(node)
-
-	for new_node in visited_nodes:
-		new_walls += get_new_walls(new_node)
-
-	return new_walls
 
 def mark_drill_visited(node):
 	node.visited = True
@@ -146,29 +180,104 @@ def mark_drill_visited(node):
 	for col_neighbor in node.entire_column:
 		col_neighbor.column_taken = True
 
-# if we drill out this node will there be a loop
-def check_drill_creates_loop(node):
-	visited_in_nodes = set(filter(lambda x:x.visited, can_reach_me(node)))
-	visited_out_nodes = set(filter(lambda x:x.visited, i_can_reach(node)))
-	return len(visited_in_nodes) > 0 and len(visited_out_nodes) > 0 and len(visited_in_nodes.union(visited_out_nodes)) > 1
 
-def check_drill_creates_unpassables(node):
-	# only check planar neighbors because switch neighbors already have column taken and cannot become un-passable hence no need to resolve them
-	filled_planar_neighbors = filter(lambda x:not x.column_taken, node.planar_neighbors)
-	for neighbor in filled_planar_neighbors:
-		is_black_block = True
-		for node_in_column in neighbor.entire_column:
-			empty_in_nodes = set(filter(lambda x:x.empty, can_reach_me(node_in_column)))
-			empty_out_nodes = set(filter(lambda x:x.empty, i_can_reach(node_in_column)))
-			if not (len(empty_in_nodes) > 0 and len(empty_out_nodes) > 0 and len(empty_in_nodes.union(empty_out_nodes)) > 1):
-				is_black_block = False
-				break
-		if is_black_block == True:
+"""
+--------------\
+SWITCH CODE 
+	    o
+	   /
+	__/__
+--------------/
+"""
+
+def is_switch_valid(start, finish, switch_color):
+	if start.visited and not finish.column_taken and not finish.visited:
+		return not (check_switch_creates_unpassables(finish, switch_color) or check_switch_creates_loop(finish, switch_color))
+	else:
+		return False
+
+def mark_switch_visited(node, switch_color):
+	node.visited = True
+	for col_neighbor in node.entire_column:
+		col_neighbor.empty = True
+		col_neighbor.column_taken = True
+		col_neighbor.is_switch = True
+		col_neighbor.switched_node = node.entire_column[col_neighbor.color ^ switch_color]
+
+def check_switch_creates_unpassables(node, switch_color):
+	column = node.entire_column
+	for color in len(column):
+		if check_creates_unpassables(column[color], column[color ^ switch_color]):
 			return True
 	return False
 
+def check_switch_creates_loop(node, switch_color):
+	column = node.entire_column
+	for color in len(column):
+		if check_creates_loop(column[color], column[color ^ switch_color]):
+			return True
+	return False
 
+def node_to_char(node):
+	empty_count = 0
+	empty_color = None
+	colors = ['e', 'R', 'G', 'Y', 'B', 'M', 'C', 'W']
+	for col_neighbor in node.entire_column:
+		if col_neighbor.empty:
+			empty_count += 1
+			empty_color = col_neighbor.color
+	if empty_count == 0:
+		# in the future there should not be any unpassable blocks at all
+		# but for now just replace them with white blocks
+		return 'w'
+	elif empty_count == 1:
+		return colors[empty_color].lower()
+	elif empty_count == 8:
+		if node.is_switch:
+			return colors[get_switch_color(node.color, node.switched_node.color)]
+		else:
+			return ' '
+	else:
+		pass
+		# print "Invalid column"
+		# print [n.empty for n in node.entire_column]
+
+def stringify_grid(grid, width, height, start=(0, 0), end=None):
+	if end is None:
+		end = (width-1, height-1)
+	level_string = ""
+	level_string += "%d %d " % (start[0], start[1])
+	level_string += "%d %d " % (end[0], end[1])
+	level_string += "%d %d " % (width, height)
+	for j in xrange(height):
+		row = ""
+		for i in xrange(width):
+			row += node_to_char(grid[i][j][0]) + ' '
+		level_string += row
+	return level_string
+
+# def build_level(width, height, start=(0, 0)):
+# 	grid = generate(width, height, 0, 0)
+# 	block, dist = pick_end_block(grid[0][0][0], width, height)
+# 	for color in xrange(8):
+# 		node = grid[block[0]][block[1]][color]
+# 		node.visited = True
+# 		node.empty = True
+# 		node.is_switch = True
+# 		node.switched_node = node
+# 	return stringify_grid(grid, width, height, start, end=block)
+
+def print_grid(grid, width, height, start=(0, 0), end=None):
+	if end is None:
+		end = (width-1, height-1)
+	print "%d %d" % (start[0], start[1])
+	print "%d %d" % (end[0], end[1])
+	print "%d %d" % (width, height)
+	for j in xrange(height):
+		row = ""
+		for i in xrange(width):
+			row += node_to_char(grid[i][j][0]) + ' '
+		print row
 
 grid = generate(4, 4, 0, 0)
-for i in xrange(4):
-	print [grid[i][j][0].empty for j in xrange(4)]
+print_grid(grid, 4, 4)
